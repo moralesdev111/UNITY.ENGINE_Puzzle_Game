f6e3f35f89d554419cafa1913cbeead0cbaf69b4
Guillermo Mora <moralesdev111@gmail.com>
2024-01-23 15:29:22 +0100

    Base Animations Done, now Laptop Canvas

---
+++ Blue Drill/Assets/Scripts/Base/DoorAnimations.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimations : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("Closing",false);
+            animator.SetBool("Opening",true);
+
+        }
+    }
+    void OnTriggerExit(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collider exited");
+            animator.SetBool("Opening",false);
+            animator.SetBool("Closing",true);
+
+        }
+    }
+}
