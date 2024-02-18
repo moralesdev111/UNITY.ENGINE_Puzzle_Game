fb9e6c848ba39d2e21fc4c58e8a1bc1bc632e4f6
Guillermo Mora <moralesdev111@gmail.com>
2024-02-18 14:28:03 +0100

    Polishing Level

--- Blue Drill/Assets/Scripts/Oxygen/BaseOxygenExclude.cs
+++ Blue Drill/Assets/Scripts/Oxygen/BaseOxygenExclude.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BaseOxygenExclude : MonoBehaviour
 {
     [SerializeField] PlayerMovement playerMovement;
     [SerializeField] Animator animator;
     public bool exclude = true;
+    [SerializeField] GameObject postProcessing;
 
+     private FogMode defaultFogMode;
+    private Color defaultFogColor;
+    private float defaultFogDensity;
+
+     private void Start()
+    {
+        // Store the default fog settings when the script starts
+        defaultFogMode = RenderSettings.fogMode;
+        defaultFogColor = RenderSettings.fogColor;
+        defaultFogDensity = RenderSettings.fogDensity;
+    }
     
     private void OnTriggerStay(Collider collider)
     {
@@ -16,7 +29,9 @@ public class BaseOxygenExclude : MonoBehaviour
             exclude = true;
             playerMovement.waterMovement = false;
             animator.SetBool("waterMovement",false);
-            
+            postProcessing.SetActive(false); 
+
+             RenderSettings.fog = false;
         }
     }
 
@@ -37,6 +52,13 @@ public class BaseOxygenExclude : MonoBehaviour
             animator.SetBool("waterMovement",true);
             AudioManager.Instance.StopMusic(0);
             AudioManager.Instance.PlayMusic("underwater",1);
+
+            postProcessing.SetActive(true);
+
+             RenderSettings.fog = true;
+            RenderSettings.fogMode = defaultFogMode;
+            RenderSettings.fogColor = defaultFogColor;
+            RenderSettings.fogDensity = defaultFogDensity;
         }
     }
     
